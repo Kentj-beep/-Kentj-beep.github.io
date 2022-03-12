1ff4f86201a0e3ed868cfbde5943656686fdfdf2
Jacob Kent <78745083+Kentj-beep@users.noreply.github.com>
2022-03-12 13:51:52 -0500

    fixed filepath

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -105,7 +105,7 @@
     function LoadHeader(): void
     {
 
-        $.get("./Views/components/header.html", function(html_data)
+        $.get("./views/components/header.html", function(html_data)
         {
             $("header").html(html_data);
             
